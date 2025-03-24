e87e14ee894ab2a6fa9631707df627a126df8904
ameer017 <rajiabdullahi907@gmail.com>
2025-03-24 21:49:05 +0100

    building a news app with news API

---
+++ script.js
@@ -0,0 +1,12 @@
+const API_KEY = "88dd43066e6a4f57990be34886ddf850";
+const API_URL = "https://newsapi.org/v2/top-headlines?";
+const newsContainer = document.getElementById("news-container");
+const searchBtn = document.getElementById("searchBtn");
+const search = document.getElementById("search");
+
+function fetchNews(sources = techCrunch) {}
+function displayNews() {}
+
+searchBtn.addEventListener("click", () => {});
+
+fetchNews();
